839e9b32b743c433210b5c9e10d39b73822fb49a
Arseniy Smirnov <aasmirnov_16@edu.hse.ru>
2024-07-20 19:55:40 +0300

    Added new Pink Paint Shader.

---
+++ advanced-animations/src/main/java/ru/edu/hse/advanced_animations/components/PinkPaint.kt
@@ -0,0 +1,45 @@
+package ru.edu.hse.advanced_animations.components
+
+import android.graphics.RenderEffect
+import android.graphics.RuntimeShader
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.asComposeRenderEffect
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.onSizeChanged
+import kotlinx.coroutines.delay
+import ru.edu.hse.advanced_animations.shaders.PINK_PAINT_SHADER
+
+fun Modifier.pinkPaintShader(): Modifier = composed {
+
+    val shader = remember { RuntimeShader(PINK_PAINT_SHADER) }
+
+    val timeMs = remember { mutableFloatStateOf(0f) }
+    LaunchedEffect(Unit) {
+        while (true) {
+            timeMs.floatValue = (System.currentTimeMillis() % 100_000L) / 1_000f
+            delay(10)
+        }
+    }
+
+    this
+        .onSizeChanged { size ->
+            shader.setFloatUniform(
+                "size",
+                size.width.toFloat(),
+                size.height.toFloat()
+            )
+        }
+        .graphicsLayer {
+            clip = true
+            shader.setFloatUniform("time", timeMs.floatValue)
+            renderEffect =
+                RenderEffect
+                    .createRuntimeShaderEffect(shader, "composable")
+                    .asComposeRenderEffect()
+        }
+}
+
